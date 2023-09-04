cc871cdea607732b29fe78d48d9996fec97da093
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2023-09-03 22:26:02 -0300

    post adicionado

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -23,6 +23,17 @@ class UsuarioController {
             return res.status(500).json(error.message)
         }
     }
+    static async criaUsuario(req, res) {
+        const novoUsuario = req.body;
+        try {
+            const novoUsuarioCriado = await database.usuario.create(novoUsuario)
+            return res.status(200).json(novoUsuarioCriado);
+        } catch (error) {
+            return res.status(500).json(error.message)
+
+        }
+    }
 }
+
 
 module.exports = UsuarioController;
--- api/routes/usuarioRoute.js
+++ api/routes/usuarioRoute.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get('/usuarios', UsuarioController.pegaTodosOsUsuarios)
 router.get('/usuarios/:id', UsuarioController.pegaUmUsuario)
+router.post('/usuarios', UsuarioController.criaUsuario)
+
 
 module.exports = router
