e7a555e308988151c95fee6a0d90151f5138dccf
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2023-09-03 22:47:35 -0300

    CRUD do usuario completo

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -30,7 +30,38 @@ class UsuarioController {
             return res.status(200).json(novoUsuarioCriado);
         } catch (error) {
             return res.status(500).json(error.message)
-
+        }
+    }
+    static async atualizaUsuario(req, res) {
+        const { id } = req.params;
+        const novaInfos = req.body;
+        try {
+            await database.usuario.update(novaInfos, {
+                where: {
+                    id: Number(id)
+                }
+            })
+            const usuarioAtualizado = await database.usuario.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(usuarioAtualizado);
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async apagaUsuario(req, res){
+        const {id} = req.params;
+        try {
+            await database.usuario.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json({mensagem:"deletado"})
+        } catch (error) {
+            return res.status(500).json(error.message)
         }
     }
 }
--- api/routes/usuarioRoute.js
+++ api/routes/usuarioRoute.js
@@ -6,6 +6,10 @@ const router = Router();
 router.get('/usuarios', UsuarioController.pegaTodosOsUsuarios)
 router.get('/usuarios/:id', UsuarioController.pegaUmUsuario)
 router.post('/usuarios', UsuarioController.criaUsuario)
+router.put('/usuarios/:id', UsuarioController.atualizaUsuario)
+router.delete('/usuarios/:id', UsuarioController.apagaUsuario)
+
+
 
 
 module.exports = router
